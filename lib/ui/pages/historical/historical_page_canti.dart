63b234279b6d997f536a93f43f0273be56139eb0
rocketikan <kazeemonitor@gmail.com>
2023-03-20 11:40:40 +0700

    historical-ui

--- lib/ui/pages/historical/historical_page_canti.dart
+++ lib/ui/pages/historical/historical_page_canti.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled/data/models/server/data.dart';
 
 class HistoricalPageCanti extends StatefulWidget {
   const HistoricalPageCanti({Key? key}) : super(key: key);
@@ -10,18 +11,51 @@ class HistoricalPageCanti extends StatefulWidget {
 
 class _HistoricalCantiState extends State<HistoricalPageCanti>
     with SingleTickerProviderStateMixin {
+  final List<Map> _books = [
+    {
+      "DATETIME": "2023-03-20T11:22:50",
+      "TS": "11:22:50",
+      "Date": "2023-03-20",
+      "tinggi": 38.55,
+      "tegangan": 13.43,
+      "suhu": 55.3,
+      "frcst30": 35.15,
+      "frcst300": 89.48,
+      "rms": 49.23,
+      "threshold": 443.07,
+      "status": "SAFE"
+    },
+    {
+      "DATETIME": "2023-03-20T11:34:59",
+      "TS": "11:34:59",
+      "Date": "2023-03-20",
+      "tinggi": 67.36,
+      "tegangan": 13.39,
+      "suhu": 54.8,
+      "frcst30": 56.91,
+      "frcst300": 63.13,
+      "rms": 56.33,
+      "threshold": 506.97,
+      "status": "SAFE"
+    },
+    {
+      "DATETIME": "2023-03-20T11:35:00",
+      "TS": "11:35:00",
+      "Date": "2023-03-20",
+      "tinggi": 59.1,
+      "tegangan": 13.39,
+      "suhu": 54.8,
+      "frcst30": 54.21,
+      "frcst300": 63.03,
+      "rms": 56.06,
+      "threshold": 504.54,
+      "status": "SAFE"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('DataTable Demo'),
-        ),
-        body: ListView(
-          children: [_createDataTable()],
-        ),
-      ),
-    );
+    return ListView(children: [_createDataTable()]);
   }
 
   DataTable _createDataTable() {
@@ -30,24 +64,21 @@ class _HistoricalCantiState extends State<HistoricalPageCanti>
 
   List<DataColumn> _createColumns() {
     return [
-      DataColumn(label: Text('ID')),
-      DataColumn(label: Text('Book')),
-      DataColumn(label: Text('Author'))
+      DataColumn(label: Text('Tanggal')),
+      DataColumn(label: Text('Waktu')),
+      DataColumn(label: Text('Ketinggian Air')),
+      DataColumn(label: Text('Forecasting Ketinggian Air(30)'))
     ];
   }
 
   List<DataRow> _createRows() {
-    return [
-      DataRow(cells: [
-        DataCell(Text('#100')),
-        DataCell(Text('Flutter Basics')),
-        DataCell(Text('David John'))
-      ]),
-      DataRow(cells: [
-        DataCell(Text('#101')),
-        DataCell(Text('Dart Internals')),
-        DataCell(Text('Alex Wick'))
-      ])
-    ];
+    return _books
+        .map((book) => DataRow(cells: [
+              DataCell(Text(book['Date'])),
+              DataCell(Text(book['TS'])),
+              DataCell(Text(book['tinggi'].toString())),
+              DataCell(Text(book['frcst30'].toString()))
+            ]))
+        .toList();
   }
 }
