6512563d429474783269a3f2adbab5594e3eb75c
emenace <emenace098@gmail.com>
2023-03-22 01:01:22 +0700

    historical: add chart to display

--- lib/ui/pages/historical/historical_page_canti.dart
+++ lib/ui/pages/historical/historical_page_canti.dart
@@ -1,14 +1,19 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:untitled/data/models/server/data.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:charts_flutter/flutter.dart' as charts;
+
+import 'data_modeling/restDataModel.dart';
+
 
 DateTime now = DateTime.now();
 Timer? timer;
+
+List<dataAPI> API = [];
+bool loading = true;  
 
 class HistoricalPageCanti extends StatefulWidget {
   const HistoricalPageCanti({Key? key}) : super(key: key);
@@ -19,15 +24,17 @@ class HistoricalPageCanti extends StatefulWidget {
 
 class _HistoricalCantiState extends State<HistoricalPageCanti>
     with SingleTickerProviderStateMixin {
-  var dataJson = [];
-
-  Future<List> getHttp100() async {
-    var url = Uri.parse('https://vps.isi-net.org/api/petengoran/latest');
-    var response = await http.read(url);
-    var jsonResponse = jsonDecode(response);
-    debugPrint(jsonResponse['result'].toString());
-    dataJson = jsonResponse['result'];
-    return jsonResponse['result'];
+  
+  final TextEditingController historyController = TextEditingController();
+  void getData() async {
+    var response = await http.get(
+        Uri.parse("https://vps.isi-net.org/api/panjang/count/200"),
+        headers: {"Accept": "application/json"});
+    List data = json.decode(response.body)['result'];
+    setState(() {
+      API = dataAPIFromJson(data);
+      loading = false;
+    });
   }
 
   @override
@@ -35,7 +42,7 @@ class _HistoricalCantiState extends State<HistoricalPageCanti>
     timer = Timer.periodic(const Duration(seconds: 1), updateDataSource);
     timer;
     super.initState();
-    getHttp100();
+    getData();
   }
 
   void updateDataSource(Timer timer) {
@@ -48,39 +55,108 @@ class _HistoricalCantiState extends State<HistoricalPageCanti>
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView(children: [_createDataTable()]);
+  // CREATE CHART BY DATA FROM REST-API
+  
+  
+  // ---------------
+  List<charts.Series<dataAPI, DateTime>> _createSampleData() {
+    return [
+      //charts.Series memiliki 4 paramter wajib
+      charts.Series<dataAPI, DateTime>(
+        data: API,
+        id: 'Water',  
+        colorFn: (_, __) => charts.MaterialPalette.purple.shadeDefault,
+        domainFn: (dataAPI DataApi, _) => DataApi.datetime,
+        measureFn: (dataAPI DataApi, _) => DataApi.waterlevel.round(),
+      )
+    ];
   }
 
-  DataTable _createDataTable() {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [
+        SizedBox(
+          height: 300,
+          child: charts.TimeSeriesChart(
+            _createSampleData(),
+            animate: false,
+        )),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: _createDataTable(),
+        ) 
+      ]);
+  }
+
+DataTable _createDataTable() {
     return DataTable(columns: _createColumns(), rows: _createRows());
   }
 
   List<DataColumn> _createColumns() {
     return [
-      DataColumn(label: Text('Tanggal & Waktu')),
-      DataColumn(label: Text('Ketinggian Air')),
-      DataColumn(label: Text('Forecasting Ketinggian Air(30)'))
+      DataColumn(
+        label: Container(
+          width: 80,
+          child: Text(
+            'Tanggal & Waktu',
+            softWrap: true,
+            textAlign: TextAlign.center,
+          ),
+        )
+      ),
+      DataColumn(
+        label: Container(
+          width: 80,
+          child: Text(
+            'Ketinggian Air',
+            softWrap: true,
+            textAlign: TextAlign.center,
+          )
+        ),
+      ),
+      DataColumn(
+        label: Container(
+        width: 80,
+        child: Text(
+          'Forecast 30',
+          softWrap: true,
+          textAlign: TextAlign.center
+          )
+        )
+      )
     ];
   }
 
   List<DataRow> _createRows() {
-    return dataJson
+    return API
         .map((data) => DataRow(cells: [
-              DataCell(Text(dateTimeParse(data['datetime']))),
-              DataCell(Text(data['waterlevel'].toString())),
-              DataCell(Text(data['forecast30'].toString()))
+              DataCell(
+                Container(
+                  width: 80,
+                  child: Text(
+                    textAlign: TextAlign.center, 
+                    DateFormat('MM/dd/yyyy hh:mm a').format(data.datetime)
+                  ),
+                )
+              ),
+              DataCell(
+                Container(
+                  width: 80,
+                  child: Text(
+                    textAlign: TextAlign.center, 
+                    data.waterlevel.toString()
+                  ),
+                )
+              ),
+              DataCell(
+                  Container(
+                    width: 80,
+                    child: Text(textAlign: TextAlign.center, 
+                    data.forecast30.toString()
+                                  ),
+                  )
+              )
             ]))
         .toList();
   }
-}
-
-String dateTimeParse(date) {
-  DateTime parseDate =
-      new DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(date);
-  var inputDate = DateTime.parse(parseDate.toString());
-  var outputFormat = DateFormat('MM/dd/yyyy hh:mm a');
-  var outputDate = outputFormat.format(inputDate);
-  return outputDate;
 }
